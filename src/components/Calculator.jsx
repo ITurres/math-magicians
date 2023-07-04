7679e34ab05dfb541467a3bba014259e7cb7686d
iturres <emanueliturres.co@gmail.com>
2023-07-04 18:45:33 -0300

    Add 'Calculator' -component- and Invoke calculator -related-component-

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,11 @@
+import CalculatorScreen from './CalculatorScreen';
+import CalculatorButtonsHolder from './CalculatorButtonsHolder';
+
+const Calculator = () => (
+  <div className="Calculator">
+    <CalculatorScreen />
+    <CalculatorButtonsHolder />
+  </div>
+);
+
+export default Calculator;
