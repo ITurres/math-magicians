c41e07ad1d16c8981c6f9976475246fdd8e442b5
iturres <emanueliturres.co@gmail.com>
2023-07-04 18:40:07 -0300

    Renamed import of 'App.jsx' -script-

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
-import App from './js/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
