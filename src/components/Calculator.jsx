7607c6011b3c347d2f6edfd83678e920395b19fa
iturres <emanueliturres.co@gmail.com>
2023-07-05 19:38:02 -0300

    Added event handler to 'Calculator' -component-

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,11 +1,32 @@
+import React, { useState } from 'react';
 import CalculatorScreen from './CalculatorScreen';
-import CalculatorButtonsHolder from './CalculatorButtonsHolder';
+import CalculatorButtons from './CalculatorButtons';
+import calculator from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="Calculator">
-    <CalculatorScreen />
-    <CalculatorButtonsHolder />
-  </div>
-);
+const Calculator = () => {
+  const currentOperation = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+
+  const [screenValue, setScreenValue] = useState(currentOperation);
+
+  const onHandleClick = (e) => {
+    const calculatorButton = e.target.innerText;
+    const newScreenValue = calculator.calculate(screenValue, calculatorButton);
+
+    setScreenValue(newScreenValue);
+  };
+
+  const screenValueToDisplay = screenValue.next || screenValue.total || 0;
+
+  return (
+    <div className="Calculator">
+      <CalculatorScreen screenValue={screenValueToDisplay} />
+      <CalculatorButtons handleClick={onHandleClick} />
+    </div>
+  );
+};
 
 export default Calculator;
