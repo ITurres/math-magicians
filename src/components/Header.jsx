4b62bc8f5dbad386d6d52d838c358e2c75e74762
iturres <emanueliturres.co@gmail.com>
2023-07-11 21:07:43 -0300

    Add 'Header' -component-

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import logo from '../assets/media/math-magicians-logo.png';
+
+const Header = () => (
+  <header>
+    <nav className="navbar container">
+      <img src={logo} width="90" height="90" alt="logo" />
+      <ul className="d-flex list-unstyled mb-0">
+        <li className="list-inline-item navbar__item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="list-inline-item navbar__item">
+          <Link to="/calculator">Calculator</Link>
+        </li>
+        <li className="list-inline-item navbar__item">
+          <Link to="/quote">Quote</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
